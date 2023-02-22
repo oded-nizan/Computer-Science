cbd33a07c20d17014d9cef573315a2c7554de553
oded-nizan <oded.nizan@gmail.com>
2023-02-22 09:50:43 +0200

    all of the house  project

---
+++ 22.02.23/classwork/src/House.java
@@ -0,0 +1,78 @@
+public class House {
+
+    String landlordName;
+    int areaCode;
+    int houseSquareFootage;
+    int tenants;
+
+    public House(String landlordName, int areaCode, int houseSquareFootage, int tenants)
+    {
+        this.landlordName = landlordName;
+        this.areaCode = areaCode;
+        this.houseSquareFootage = houseSquareFootage;
+        this.tenants = tenants;
+    }
+
+    public String getLandlordName()
+    {
+        return this.landlordName;
+    }
+
+    public int getAreaCode()
+    {
+        return this.areaCode;
+    }
+
+    public int getHouseSquareFootage()
+    {
+        return this.houseSquareFootage;
+    }
+
+    public int getTenants()
+    {
+        return this.tenants;
+    }
+
+    public void setLandlordName(String landlordName)
+    {
+        this.landlordName = landlordName;
+    }
+
+    public void setAreaCode(int areaCode)
+    {
+        if (areaCode > 0 && areaCode < 6)
+        {
+            this.areaCode = areaCode;
+        }
+    }
+
+    public void setHouseSquareFootage(int houseSquareFootage)
+    {
+        this.houseSquareFootage = houseSquareFootage;
+    }
+
+    public void setTenants(int tenants)
+    {
+        this.tenants = tenants;
+    }
+
+    public int propertyTax()
+    {
+        return this.houseSquareFootage * this.areaCode * 50;
+    }
+
+    public double density()
+    {
+        //noinspection IntegerDivisionInFloatingPointContext
+        return this.houseSquareFootage / this.tenants;
+    }
+
+    public String toString()
+    {
+        String toString = "The name of the landlord is : " + this.landlordName + "\n";
+        toString = toString +  "The area code is : " + this.areaCode + "\n";
+        toString = toString + "The house square footage is : " + this.houseSquareFootage + "\n";
+        toString = toString + "The number pf tenants in the house is : " + this.tenants + "\n";
+        return toString;
+    }
+}
---
+++ 22.02.23/classwork/src/Main.java
@@ -0,0 +1,84 @@
+public class Main
+{
+
+
+    public static void printHouseArray(House[] arrH)
+    {
+        for (int i = 0; i < arrH.length; i++)
+        {
+            System.out.println(arrH[i].toString());
+        }
+    }
+
+    public static int housesInArea(House[] arrH, int areaCode)
+    {
+        int m = 0;
+        for (int i = 0; i < arrH.length; i++)
+        {
+            if (arrH[i].areaCode == areaCode)
+            {
+                m++;
+            }
+        }
+        return m;
+    }
+
+    public static double squareFootageAverage(House[] arrH, int areaCode)
+    {
+        int sum = 0;
+        int avg;
+        int m = housesInArea(arrH, areaCode);
+        for (int i = 0; i < arrH.length; i++)
+        {
+            if (arrH[i].areaCode == areaCode)
+            {
+                sum += arrH[i].houseSquareFootage;
+            }
+        }
+        avg = sum / m;
+        return avg;
+    }
+
+    public static int highestSquareFootageAverage (House[] arrH)
+    {
+        int max = 0;
+        int a;
+        for (int i = 0; i < arrH.length; i++)
+        {
+            a = (int)squareFootageAverage(arrH, i);
+            max = Math.max(a, max);
+        }
+        return max;
+    }
+
+    public static void main(String[] args)
+    {
+
+        House h1 = new House("Roy", 5, 900, 3);
+
+        System.out.println(h1.getLandlordName());
+        System.out.println(h1.getAreaCode());
+        System.out.println(h1.getHouseSquareFootage());
+        System.out.println(h1.getTenants());
+
+        h1.setLandlordName("Roe");
+        h1.setAreaCode(2);
+        h1.setHouseSquareFootage(10000);
+        h1.setTenants(5);
+
+        System.out.println("Your property tax is  "  + h1.propertyTax());
+        System.out.println("Your house's density is : " + h1.density());
+
+        House[] arrH = new House[4];
+        arrH[0] = new House("Roy1", 1, 70, 2);
+        arrH[1] = new House("Roy2", 2, 80, 3);
+        arrH[2] = new House("Roy3", 3, 90, 4);
+        arrH[3] = new House("Roy4", 4, 100, 5);
+
+        printHouseArray(arrH);
+
+        System.out.println(squareFootageAverage(arrH, 1));
+
+        System.out.println(highestSquareFootageAverage(arrH));
+    }
+}
