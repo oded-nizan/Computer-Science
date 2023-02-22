878223b9b3b4e68f67f587680fe38f88211e584b
oded-nizan <oded.nizan@gmail.com>
2023-02-22 09:59:13 +0200

    all of thee sniper  project

---
+++ 20.02.23/classwork/src/Sniper.java
@@ -0,0 +1,61 @@
+public class Sniper {
+
+    private int id;
+    private int code;
+    private int shootsFired;
+    private int shootsOnTarget;
+
+    public Sniper(int id, int code, int shootsFired, int shootsOnTarget)
+    {
+        this.id = id;
+        this.code = code;
+        this.shootsFired = shootsFired;
+        this.shootsOnTarget = shootsOnTarget;
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public int getCode()
+    {
+        return this.code;
+    }
+
+    public int getShootsFired()
+    {
+        return this.shootsFired;
+    }
+
+    public int getShootsOnTarget()
+    {
+        return this.shootsOnTarget;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public void setCode(int code)
+    {
+        this.code = code;
+    }
+
+    public void  setShootsFired(int shootsFired)
+    {
+        this.shootsFired = shootsFired;
+    }
+
+    public void setShootsOnTarget(int shootsOnTarget)
+    {
+        this.shootsOnTarget = shootsOnTarget;
+    }
+
+    public String toString()
+    {
+        String toString = "The sniper's id is : " + this.id + "\n";
+        toString =toString + "The sniper's code is : " + this.code + "\n";
+    }
+}
