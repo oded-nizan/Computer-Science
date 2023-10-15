31f2580135449db85776f338985fde54abf7a145
oded-nizan <odednizan@gmail.com>
2023-10-15 10:57:13 +0300

    15.10.2023 #2

--- 10Grade/15.10.2023/classwork/src/Main.java
+++ 10Grade/15.10.2023/classwork/src/Main.java
@@ -1,27 +1,26 @@
-public static void main(String[] args)
-        {
+public class Main {
+
+    public static void main(String[] args) {
         System.out.println("Hello world!");
-        int[] arr = {6,13,2,9,27,10};
+        int[] arr = {6, 13, 2, 9, 27, 10};
         Queue<Integer> q1 = buildQ(arr);
         printQ(q1);
-        }
+    }
 
 
-public static Queue<Integer> buildQ(int [] a)
-        {
+    public static Queue<Integer> buildQ(int[] a) {
         Queue<Integer> q = new Queue<Integer>();
-        for(int i=0;i<a.length;i++)
-        q.insert(a[i]);
+        for (int i = 0; i < a.length; i++)
+            q.insert(a[i]);
         return q;
-        }
+    }
 
-public static void printQ(Queue<Integer> q)
-        {
+    public static void printQ(Queue<Integer> q) {
         System.out.println("the q");
-        while(q.isEmpty()==false)
-        {
-        int x = q.remove();
-        System.out.print(x+" ");
+        while (!q.isEmpty()) {
+            int x = q.remove();
+            System.out.print(x + " ");
         }
         System.out.println();
-        }
+    }
+}
