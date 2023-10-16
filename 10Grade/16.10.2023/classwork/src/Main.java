041517adf2dc8dd1508cb644be5d38dcc40812b8
oded-nizan <odednizan@gmail.com>
2023-10-16 11:32:30 +0300

    16.10.2023 #1

---
+++ 10Grade/16.10.2023/classwork/src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+
+    public static int factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * factorial(n-1);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(factorial(5));
+    }
+}
