7ef7e644a537701f767e919be334654c37b35c20
oded-nizan <odednizan@gmail.com>
2023-10-16 11:58:55 +0300

    16.10.2023 #2

--- 10Grade/16.10.2023/classwork/src/Main.java
+++ 10Grade/16.10.2023/classwork/src/Main.java
@@ -13,8 +13,32 @@ public class Main
         }
     }
 
+    public static int sum(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return n + sum(n-1);
+        }
+    }
+
+    public static int multiply(int n, int m)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return m + multiply(n-1, m);
+        }
+    }
+
     public static void main(String[] args)
     {
-        System.out.println(factorial(5));
+        System.out.println(multiply(4, 5));
     }
 }
