951aaf3a78d31269e2d4b78867a3f2fa6cb9ced7
oded-nizan <odednizan@gmail.com>
2023-11-15 15:16:30 +0200

    15.11.2023 #5

---
+++ 10Grade/Assembly/15.11.2023.asm
@@ -0,0 +1,62 @@
+IDEAL
+MODEL small
+STACK 100h
+DATASEG
+; ------------------------
+; Your variables here
+
+var db 12
+var1 db 15
+
+; ------------------------
+
+CODESEG
+start:
+    mov ax, @data
+    mov ds, ax
+
+; ------------------------
+; Your code here
+
+mov ax, 10 ; ax = 10
+mov bx, xc ; bx = cx
+mov bx, 4 ; bx = 4
+add ax, bx ; ax = ax + bx
+sub bx, dx ; bx = bx - dx
+
+mov ax, 7
+sub ax, 5
+mov bx, 3
+mul bl
+add ax, 8
+
+mov ax, 4
+add ax, 5
+mov bx, 3
+sub bx, 1
+mul bl
+
+mov ac, 0abh
+mov bx, 0cdh
+add ax, bx
+
+mov ax, 3
+add ax, 2
+mov bx, 4
+mul bl
+
+mov ax, 8
+sub ax, 5
+mov bx, 3
+mul bl
+mov xc, 5
+add cd, 2
+add ax, cx
+
+; ------------------------
+
+exit:
+    mov ax, 4c00h
+    int 21h
+
+END start
