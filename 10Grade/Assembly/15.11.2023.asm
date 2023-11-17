8f4de255239f02abcfa2bc5e8746e17b639ab1cc
oded-nizan <odednizan@gmail.com>
2023-11-17 14:58:10 +0200

    17.11.2023 #1

--- 10Grade/Assembly/15.11.2023.asm
+++ 10Grade/Assembly/15.11.2023.asm
@@ -18,32 +18,32 @@ start:
 ; ------------------------
 ; Your code here
 
-mov ax, 10 ; ax = 10
-mov bx, xc ; bx = cx
-mov bx, 4 ; bx = 4
-add ax, bx ; ax = ax + bx
-sub bx, dx ; bx = bx - dx
+;mov ax, 10 ; ax = 10
+;mov bx, xc ; bx = cx
+;mov bx, 4 ; bx = 4
+;add ax, bx ; ax = ax + bx
+;sub bx, dx ; bx = bx - dx
 
-mov ax, 7
-sub ax, 5
-mov bx, 3
-mul bl
-add ax, 8
+;mov ax, 7
+;sub ax, 5
+;mov bx, 3
+;mul bl
+;add ax, 8
 
-mov ax, 4
-add ax, 5
-mov bx, 3
-sub bx, 1
-mul bl
+;mov ax, 4
+;add ax, 5
+;mov bx, 3
+;sub bx, 1
+;mul bl
 
-mov ax, 0abh
-mov bx, 0cdh
-add ax, bx
+;mov ax, 0abh
+;mov bx, 0cdh
+;add ax, bx
 
-mov ax, 3
-add ax, 2
-mov bx, 4
-mul bl
+;mov ax, 3
+;add ax, 2
+;mov bx, 4
+;mul bl
 
 mov ax, 8
 sub ax, 5
